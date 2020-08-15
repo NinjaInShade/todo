ca543bb4b75bd57e316f7939e22b38eaae970ef8
Leon Michalak <leonmichalak6@gmail.com>
2020-08-15 09:21:16 +0100

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Home />
+    </React.Fragment>
+  );
+}
+
+export default App;
