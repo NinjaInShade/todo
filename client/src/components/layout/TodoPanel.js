882f78dd240d727242247f70730a1a47506ba4c5
Leon Michalak <leonmichalak6@gmail.com>
2021-03-09 11:43:39 +0000

    filter UI complete

--- client/src/components/layout/TodoPanel.js
+++ client/src/components/layout/TodoPanel.js
@@ -7,6 +7,13 @@ import "./TodoPanel.css";
 
 export default function TodoPanel({ loading, setTodos, todos, theme }) {
   const [value, setValue] = useState("");
+  const [filter, setFilter] = useState("All");
+
+  let itemsLeft;
+
+  if (!loading) {
+    itemsLeft = todos.todos.filter((todoState) => todoState.completed === false).length;
+  }
 
   function createTodo() {
     fetch(process.env.REACT_APP_API_URL, {
@@ -26,6 +33,10 @@ export default function TodoPanel({ loading, setTodos, todos, theme }) {
       });
   }
 
+  function clearCompleted(e) {
+    e.preventDefault();
+  }
+
   return (
     <>
       <form className={`todo-section add-form ${theme === "dark" ? "todo-section-dark" : ""}`}>
@@ -43,16 +54,35 @@ export default function TodoPanel({ loading, setTodos, todos, theme }) {
         />
       </form>
       <ul className="todos">
-        {!loading ? (
-          todos.todos.map((todo, index) => {
-            return <TodoSection todo={todo} theme={theme} key={index} borderRadius={index === 0} todos={todos} setTodos={setTodos} />;
-          })
-        ) : (
+        {loading && (
           <div className={`loading ${theme === "dark" ? "loading-dark" : ""}`}>
             <LoadingSpinner theme={theme} />
           </div>
         )}
-        <div className={`todos-filter ${theme === "dark" ? "todos-filter-dark" : ""}`}></div>
+        {!loading &&
+          todos.todos.map((todo, index) => {
+            return <TodoSection todo={todo} theme={theme} key={index} borderRadius={index === 0} todos={todos} setTodos={setTodos} />;
+          })}
+        <div className={`todos-filter ${theme === "dark" ? "todos-filter-dark" : ""}`}>
+          <p className={`todo-items ${theme === "dark" ? "todo-items-dark" : ""}`}>{itemsLeft} items left</p>
+          <div className="filters-container">
+            <button onClick={() => setFilter("All")}>
+              {" "}
+              <p className={`todo-filter ${theme === "dark" ? "todo-filter-dark" : ""} ${filter === "All" ? "todo-filter-active" : ""}`}>All</p>
+            </button>
+            <button onClick={() => setFilter("Active")}>
+              <p className={`todo-filter ${theme === "dark" ? "todo-filter-dark" : ""} ${filter === "Active" ? "todo-filter-active" : ""}`}>Active</p>
+            </button>
+            <button onClick={() => setFilter("Completed")}>
+              <p className={`todo-filter ${theme === "dark" ? "todo-filter-dark" : ""} ${filter === "Completed" ? "todo-filter-active" : ""}`}>
+                Completed
+              </p>
+            </button>
+          </div>
+          <button onClick={(e) => clearCompleted(e)}>
+            <p className={`todo-clear ${theme === "dark" ? "todo-clear-dark" : ""}`}>Clear Completed</p>
+          </button>
+        </div>
       </ul>
     </>
   );
