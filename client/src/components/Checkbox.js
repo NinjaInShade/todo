ae39b77b416c4d66243ac650be7f9b201e62825e
Leon Michalak <leonmichalak6@gmail.com>
2021-07-02 10:48:44 +0100

    Fixed bug where after dragging an in-completed todo in the place of a completed one, it would style as checked. Also removed unneeded timeout

--- client/src/components/Checkbox.js
+++ client/src/components/Checkbox.js
@@ -1,37 +1,26 @@
-import React, { useState } from "react";
-import checkedIcon from "../assets/icon-check.svg";
+import React from 'react';
+import checkedIcon from '../assets/icon-check.svg';
 
-import "./Checkbox.css";
+import './Checkbox.css';
 
 export default function Checkbox({ onClick, add, theme, active }) {
-  const [checked, setChecked] = useState(active);
-
   function toggleCheckbox(e) {
     e.preventDefault();
-
-    // Toggle checked state (if we're adding a todo we want to show checked state for a tiny bit)
-    setChecked(!checked);
-
-    if (add) {
-      setTimeout(() => {
-        setChecked(false);
-      }, 100);
-    }
 
     // Run external onClick function (create or complete the todo)
     onClick();
   }
 
   return (
-    <div className="checkbox-container">
+    <div className='checkbox-container'>
       <button
-        className={`custom-checkbox ${theme === "dark" ? "custom-checkbox-dark" : ""}`}
+        className={`custom-checkbox ${theme === 'dark' ? 'custom-checkbox-dark' : ''}`}
         onClick={(e) => toggleCheckbox(e)}
-        aria-labelledby={add ? "Create new todo" : "Complete todo"}
-        type="submit"
-        style={checked || active ? { background: "var(--primary-check-background)", border: "none", padding: "5px 7px" } : {}}
+        aria-labelledby={add ? 'Create new todo' : 'Complete todo'}
+        type='submit'
+        style={active ? { background: 'var(--primary-check-background)', border: 'none', padding: '5px 7px' } : {}}
       >
-        <img src={checkedIcon} alt="checkbox checked icon" style={checked ? {} : { visibility: "hidden" }} />
+        <img src={checkedIcon} alt='checkbox checked icon' style={active ? {} : { visibility: 'hidden' }} />
       </button>
     </div>
   );
