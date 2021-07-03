293e8acdb80c84941e311e41f3ff803972d4d380
Leon Michalak <leonmichalak6@gmail.com>
2021-07-03 14:41:59 +0100

    Improved accessibility. Toggle button has appropriate aria label

--- client/src/components/layout/Content.js
+++ client/src/components/layout/Content.js
@@ -1,17 +1,17 @@
-import React from "react";
-import TodoPanel from "./TodoPanel.js";
-import moon from "../../assets/icon-moon.svg";
-import sun from "../../assets/icon-sun.svg";
+import React from 'react';
+import TodoPanel from './TodoPanel.js';
+import moon from '../../assets/icon-moon.svg';
+import sun from '../../assets/icon-sun.svg';
 
-import "./Content.css";
+import './Content.css';
 
 export default function Content({ theme, changeHandler, todos, setTodos, loading }) {
   return (
-    <section className="content-container" id="content">
-      <header className="header">
-        <h1 className="title">TODO</h1>
-        <button className="change-theme" onClick={changeHandler} tabIndex="0" aria-labelledby="Change theme button">
-          <img src={theme === "dark" ? sun : moon} alt="change theme icon" />
+    <section className='content-container' id='content'>
+      <header className='header'>
+        <h1 className='title'>TODO</h1>
+        <button className='change-theme' onClick={changeHandler} tabIndex='0' aria-label='Change theme button'>
+          <img src={theme === 'dark' ? sun : moon} alt='change theme icon' />
         </button>
       </header>
       <TodoPanel todos={todos} setTodos={setTodos} loading={loading} theme={theme} />
